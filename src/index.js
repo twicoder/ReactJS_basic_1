d5ffe417bdf82c1ad8bc415914ab3c3df491496e
twicoder <renqingwei123@163.com>
2016-11-11 17:14:48 +0800

    basics of ReactJS

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
+import NameForm from './NameForm';
+
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('root2')
+);
+
+import EssayForm from './EssayForm';
+
+ReactDOM.render(
+  <EssayForm />,
+  document.getElementById('root3')
+);
+
+import FlavorForm from './FlavorForm';
+
+ReactDOM.render(
+  <FlavorForm />,
+  document.getElementById('root4')
+);
+
+import Calculator from './Calculator';
+
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById('root5')
+);
+
+import Calculator2 from './Calculator2';
+
+ReactDOM.render(
+  <Calculator2 />,
+  document.getElementById('root6')
+);
